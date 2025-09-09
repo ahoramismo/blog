d63d2a641881df37c6cbb642e1ffafb00e2fe4b1
ahoramismo <holamundo@kakao.com>
2025-09-09 19:00:30 +0900

    feat: add blank about page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'About',
+  description: 'Who am I?',
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-4xl mb-8 tracking-tighter">About</h1>
+      To be added.
+    </section>
+  )
+}
